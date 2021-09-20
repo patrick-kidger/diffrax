d9f5e543782fdaf22301e77a8969d6c49662dec2
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2021-09-20 10:52:34 +0100

    reverting neural SDE to SDE-GAN only

--- examples/neural_sde.py
+++ examples/neural_sde.py
@@ -1,6 +1,3 @@
-import functools as ft
-import pathlib
-
 import diffrax
 import equinox as eqx
 import fire
@@ -13,9 +10,6 @@ import optax
 import tqdm
 
 
-_here = pathlib.Path(__file__).resolve().parent
-
-
 def lipswish(x):
     return 0.909 * jnn.silu(x)
 
@@ -23,10 +17,10 @@ def lipswish(x):
 class VectorField(eqx.Module):
     mlp: eqx.nn.MLP
 
-    def __init__(self, *, extra_size, hidden_size, width_size, depth, key):
+    def __init__(self, hidden_size, width_size, depth, *, key):
         super().__init__()
         self.mlp = eqx.nn.MLP(
-            in_size=hidden_size + extra_size + 1,
+            in_size=hidden_size + 1,
             out_size=hidden_size,
             width_size=width_size,
             depth=depth,
@@ -37,6 +31,7 @@ class VectorField(eqx.Module):
 
     @eqx.filter_jit
     def __call__(self, t, y, args):
+        del args
         return self.mlp(jnp.concatenate([t[None], y]))
 
 
@@ -45,7 +40,7 @@ class ControlledVectorField(eqx.Module):
     control_size: int
     hidden_size: int
 
-    def __init__(self, *, control_size, hidden_size, width_size, depth, key):
+    def __init__(self, control_size, hidden_size, width_size, depth, *, key):
         super().__init__()
         self.control_size = control_size
         self.hidden_size = hidden_size
@@ -61,189 +56,133 @@ class ControlledVectorField(eqx.Module):
 
     @eqx.filter_jit
     def __call__(self, t, y, args):
+        del args
         return self.mlp(jnp.concatenate([t[None], y])).reshape(
             self.hidden_size, self.control_size
         )
 
 
-class NeuralSDE(eqx.Module):
+class DifferentialEquation(eqx.Module):
     initial: eqx.nn.MLP
-    drift: VectorField
-    diffusion: ControlledVectorField
+    vf: VectorField  # drift
+    cvf: ControlledVectorField  # diffusion
     readout: eqx.nn.Linear
-    auxiliary_drift: VectorField
 
+    def __init__(
+        self,
+        initial_size,
+        control_size,
+        hidden_size,
+        width_size,
+        depth,
+        readout_size,
+        *,
+        key,
+    ):
+        super().__init__()
+        initial_key, vf_key, cvf_key, readout_key = jrandom.split(key, 4)
+
+        self.initial = eqx.nn.MLP(
+            initial_size, hidden_size, width_size, depth, key=initial_key
+        )
+        self.vf = VectorField(hidden_size, width_size, depth, key=vf_key)
+        self.cvf = ControlledVectorField(
+            control_size, hidden_size, width_size, depth, key=cvf_key
+        )
+        self.readout = eqx.nn.Linear(hidden_size, readout_size, key=readout_key)
+
+    @eqx.filter_jit
+    def _initial(self, ts, init):
+        t0 = ts[0]
+        t1 = ts[-1]
+        y0 = self.initial(init)
+        dt0 = 1.0
+        return t0, t1, y0, dt0
+
+    @eqx.filter_jit
+    def _readout(self, ys):
+        return jax.vmap(self.readout)(ys)
+
+    def __call__(self, ts, init, control, saveat):
+        vf = diffrax.ODETerm(self.vf)
+        cvf = diffrax.ControlTerm(self.cvf, control)
+        term = diffrax.MultiTerm((vf, cvf))
+        solver = diffrax.ReversibleHeun(term)
+
+        t0, t1, y0, dt0 = self._initial(ts, init)
+        sol = diffrax.diffeqsolve(solver, t0, t1, y0, dt0, saveat=saveat)
+        return self._readout(sol.ys)
+
+
+class NeuralSDE(eqx.Module):
+    diffeq: DifferentialEquation
     initial_noise_size: int
     noise_size: int
 
     def __init__(
         self,
-        *,
         data_size,
         initial_noise_size,
         noise_size,
         hidden_size,
         width_size,
         depth,
+        *,
         key,
     ):
         super().__init__()
-        (
-            initial_key,
-            drift_key,
-            diffusion_key,
-            readout_key,
-            aux_drift_key,
-        ) = jrandom.split(key, 5)
-
-        self.initial = eqx.nn.MLP(
-            initial_noise_size, hidden_size, width_size, depth, key=initial_key
-        )
-        self.drift = VectorField(
-            extra_size=0,
-            hidden_size=hidden_size,
-            width_size=width_size,
-            depth=depth,
-            key=drift_key,
-        )
-        self.diffusion = ControlledVectorField(
+        self.diffeq = DifferentialEquation(
+            initial_size=initial_noise_size,
             control_size=noise_size,
             hidden_size=hidden_size,
             width_size=width_size,
             depth=depth,
-            key=diffusion_key,
+            readout_size=data_size,
+            key=key,
         )
-        self.readout = eqx.nn.Linear(
-            in_features=hidden_size, out_features=data_size, key=readout_key
-        )
-        self.auxiliary_drift = VectorField(
-            extra_size=data_size,
-            hidden_size=hidden_size,
-            width_size=width_size,
-            depth=depth,
-            key=aux_drift_key,
-        )
-
         self.initial_noise_size = initial_noise_size
         self.noise_size = noise_size
 
     @eqx.filter_jit
-    def _initial(self, ts, ys, key):
-        bm_key, init_key = jrandom.split(key, 2)
-        bm = diffrax.UnsafeBrownianPath(bm_key, (self.noise_size,))
+    def _setup(self, ts, key):
+        init_key, bm_key = jrandom.split(key, 2)
         init = jrandom.normal(init_key, (self.initial_noise_size,))
-        y0 = self.initial(init)
-
-        if ys is None:
-            drift = self.drift
-            diffusion = self.diffusion
-        else:
-            interp_ys = diffrax.linear_interpolation(
-                ts, ys, replace_nans_at_start=0.0, fill_forward_nans_at_end=True
-            )
-            context = diffrax.LinearInterpolation(ts, interp_ys).evaluate
-            drift, diffusion, y0, bm = diffrax.sde_kl_divergence(
-                drift1=self.auxiliary_drift,
-                drift2=self.drift,
-                diffusion=self.diffusion,
-                context=context,
-                y0=y0,
-                bm=bm,
-            )
-        drift = diffrax.ODETerm(drift)
-        diffusion = diffrax.ControlTerm(diffusion, bm)
-        term = diffrax.MultiTerm((drift, diffusion))
-        solver = diffrax.ReversibleHeun(term)
-
-        t0 = ts[0]
-        t1 = ts[-1]
-        dt0 = 1.0
+        control = diffrax.UnsafeBrownianPath(bm_key, (self.noise_size,))
         saveat = diffrax.SaveAt(ts=ts)
-        return solver, t0, t1, y0, dt0, saveat
+        return ts, init, control, saveat
 
-    @eqx.filter_jit
-    def _sample(self, sol):
-        return jax.vmap(self.readout)(sol.ys)
-
-    def sample(self, ts, *, key):
-        solver, t0, t1, y0, dt0, saveat = self._initial(ts, None, key)
-        sol = diffrax.diffeqsolve(solver, t0, t1, y0, dt0, saveat=saveat)
-        return self._sample(sol)
-
-    def loss_gan(self, ts, ys, discriminator, *, key):
-        sample_ys = self.sample(ts, key=key)
-        real_score = discriminator(ts, ys)
-        fake_score = discriminator(ts, sample_ys)
-        return jnp.mean(real_score - fake_score)
-
-    @eqx.filter_jit
-    def _loss_vae(self, sol_with_kl, ys):
-        hidden, kl_divergence = sol_with_kl.ys
-        sample_ys = jax.vmap(self.readout)(hidden)
-        kl_divergence = kl_divergence[-1]
-        isnan = jnp.isnan(ys)
-        sample_ys = jnp.where(isnan, 0, sample_ys)
-        ys = jnp.where(isnan, 0, ys)
-        return jnp.sum((sample_ys - ys) ** 2) + kl_divergence
-
-    def loss_vae(self, ts, ys, *, key):
-        solver, t0, t1, y0, dt0, saveat = self._initial(ts, ys, key)
-        sol_with_kl = diffrax.diffeqsolve(solver, t0, t1, y0, dt0, saveat=saveat)
-        return self._loss_vae(sol_with_kl, ys)
+    def __call__(self, ts, *, key):
+        return self.diffeq(*self._setup(ts, key))
 
 
 class NeuralCDE(eqx.Module):
-    initial: eqx.nn.MLP
-    cvf: ControlledVectorField
-    readout: eqx.nn.Linear
+    diffeq: DifferentialEquation
 
-    def __init__(self, *, data_size, hidden_size, width_size, depth, key):
+    def __init__(self, data_size, hidden_size, width_size, depth, *, key):
         super().__init__()
-        initial_key, cvf_key, readout_key = jrandom.split(key, 3)
-
-        self.initial = eqx.nn.MLP(
-            in_size=data_size + 1,
-            out_size=hidden_size,
-            width_size=width_size,
-            depth=depth,
-            key=initial_key,
-        )
-        self.cvf = ControlledVectorField(
-            control_size=data_size + 1,
+        self.diffeq = DifferentialEquation(
+            initial_size=data_size + 1,
+            control_size=data_size,
             hidden_size=hidden_size,
             width_size=width_size,
             depth=depth,
-            key=cvf_key,
-        )
-        self.readout = eqx.nn.Linear(
-            in_features=hidden_size, out_features=1, key=readout_key
+            readout_size=1,
+            key=key,
         )
 
+    @staticmethod
     @eqx.filter_jit
-    def _initial(self, ts, ys):
-        ys = jnp.concatenate([ts[:, None], ys], axis=1)
+    def _setup(ts, ys):
         ys = diffrax.linear_interpolation(
             ts, ys, replace_nans_at_start=0.0, fill_forward_nans_at_end=True
         )
+        init = jnp.concatenate([ts[0, None], ys[0]])
         control = diffrax.LinearInterpolation(ts, ys)
-        y0 = self.initial(ys[0])
-
-        term = diffrax.ControlTerm(self.cvf, control)
-        solver = diffrax.ReversibleHeun(term)
-
-        t0 = ts[0]
-        t1 = ts[-1]
-        dt0 = 1.0
-        return solver, t0, t1, y0, dt0
-
-    @eqx.filter_jit
-    def _readout(self, sol):
-        return self.readout(sol.ys[-1])
+        saveat = diffrax.SaveAt(t1=True)
+        return ts, init, control, saveat
 
     def __call__(self, ts, ys):
-        solver, t0, t1, y0, dt0 = self._initial(ts, ys)
-        sol = diffrax.diffeqsolve(solver, t0, t1, y0, dt0)
-        return self._readout(sol)
+        return self.diffeq(*self._setup(ts, ys))
 
     @eqx.filter_jit
     def clip_weights(self):
@@ -311,25 +250,20 @@ def make_dataloader(arrays, batch_size, loop, *, key):
             break
 
 
-@eqx.filter_grad
-def loss(g_d, ts_i, ys_i, *, gan, vae, key, step):
-    generator, discriminator = g_d  # Just the first argument is differentiated
+def loss(generator, discriminator, ts_i, ys_i, key, step=0):
     batch_size, _ = ts_i.shape
     key = jrandom.fold_in(key, step)
     key = jrandom.split(key, batch_size)
-    if vae:
-        vae_loss = jax.vmap(generator.loss_vae)(ts_i, ys_i, key=key)
-        vae_loss = jnp.mean(vae_loss)
-    else:
-        vae_loss = 0
-    if gan:
-        gan_loss = jax.vmap(
-            ft.partial(generator.loss_gan, discriminator=discriminator)
-        )(ts_i, ys_i, key=key)
-        gan_loss = jnp.mean(gan_loss)
-    else:
-        gan_loss = 0
-    return vae_loss + gan_loss
+    fake_ys_i = jax.vmap(generator)(ts_i, key=key)
+    real_score = jax.vmap(discriminator)(ts_i, ys_i)
+    fake_score = jax.vmap(discriminator)(ts_i, fake_ys_i)
+    return jnp.mean(real_score - fake_score)
+
+
+@eqx.filter_grad
+def grad_loss(g_d, ts_i, ys_i, key, step):
+    generator, discriminator = g_d  # We differentiate just the first argument
+    return loss(generator, discriminator, ts_i, ys_i, key, step)
 
 
 @eqx.filter_jit
@@ -340,9 +274,33 @@ def update(
     d_updates, d_opt_state = d_optim.update(d_grad, d_opt_state)
     generator = eqx.apply_updates(generator, g_updates)
     discriminator = eqx.apply_updates(discriminator, d_updates)
-    if discriminator is not None:
-        discriminator = discriminator.clip_weights()
+    discriminator = discriminator.clip_weights()
     return generator, discriminator, g_opt_state, d_opt_state
+
+
+def make_step(
+    generator,
+    discriminator,
+    g_opt_state,
+    d_opt_state,
+    g_optim,
+    d_optim,
+    ts_i,
+    ys_i,
+    step,
+    key,
+):
+    g_grad, d_grad = grad_loss((generator, discriminator), ts_i, ys_i, key, step)
+    return update(
+        generator,
+        discriminator,
+        g_opt_state,
+        d_opt_state,
+        g_optim,
+        d_optim,
+        g_grad,
+        d_grad,
+    )
 
 
 def main(
@@ -351,21 +309,14 @@ def main(
     hidden_size=16,
     width_size=16,
     depth=1,
-    generator_lr=2e-5,
-    discriminator_lr=1e-4,
+    generator_lr=2e-4,
+    discriminator_lr=1e-3,
     batch_size=1024,
-    steps=3000,
+    steps=10000,
     steps_per_print=10,
     dataset_size=8192,
-    out_path=_here / "neural_sde_samples.png",
     seed=5678,
-    gan=True,
-    vae=True,
 ):
-    if not gan and not vae:
-        raise ValueError(
-            "Must train with respect to either or both of VAE and GAN losses."
-        )
 
     key = jrandom.PRNGKey(seed)
     (
@@ -378,30 +329,22 @@ def main(
         sample_key,
     ) = jrandom.split(key, 7)
     data_key = jrandom.split(data_key, dataset_size)
-    evaluate_key = jrandom.split(evaluate_key, batch_size)
 
     ts, ys = jax.vmap(get_data)(data_key)
     _, _, data_size = ys.shape
 
     generator = NeuralSDE(
-        data_size=data_size,
-        initial_noise_size=initial_noise_size,
-        noise_size=noise_size,
-        hidden_size=hidden_size,
-        width_size=width_size,
-        depth=depth,
+        data_size,
+        initial_noise_size,
+        noise_size,
+        hidden_size,
+        width_size,
+        depth,
         key=generator_key,
     )
-    if gan:
-        discriminator = NeuralCDE(
-            data_size=data_size,
-            hidden_size=hidden_size,
-            width_size=width_size,
-            depth=depth,
-            key=discriminator_key,
-        )
-    else:
-        discriminator = None
+    discriminator = NeuralCDE(
+        data_size, hidden_size, width_size, depth, key=discriminator_key
+    )
 
     g_optim = optax.rmsprop(generator_lr)
     d_optim = optax.rmsprop(-discriminator_lr)
@@ -414,52 +357,29 @@ def main(
     )
 
     for step, (ts_i, ys_i) in zip(trange, infinite_dataloader):
-        g_grad, d_grad = loss(
-            (generator, discriminator),
-            ts_i,
-            ys_i,
-            gan=gan,
-            vae=vae,
-            key=train_key,
-            step=step,
-        )
-        generator, discriminator, g_opt_state, d_opt_state = update(
+        generator, discriminator, g_opt_state, d_opt_state = make_step(
             generator,
             discriminator,
             g_opt_state,
             d_opt_state,
             g_optim,
             d_optim,
-            g_grad,
-            d_grad,
+            ts_i,
+            ys_i,
+            step,
+            train_key,
         )
 
         if (step % steps_per_print) == 0 or step == steps - 1:
-            total_vae_loss = 0
-            total_gan_loss = 0
+            total_score = 0
             num_batches = 0
             for ts_i, ys_i in make_dataloader(
-                (ts, ys), batch_size, loop=False, key=dataloader_key
+                (ts, ys), batch_size, loop=False, key=evaluate_key
             ):
-                if vae:
-                    vae_loss = jax.vmap(generator.loss_vae)(
-                        ts_i, ys_i, key=evaluate_key
-                    )
-                    total_vae_loss += jnp.mean(vae_loss).item()
-                if gan:
-                    gan_loss = jax.vmap(
-                        ft.partial(generator.loss_gan, discriminator=discriminator)
-                    )(ts_i, ys_i, key=evaluate_key)
-                    total_gan_loss += jnp.mean(gan_loss).item()
+                score = loss(generator, discriminator, ts_i, ys_i, sample_key)
+                total_score += score.item()
                 num_batches += 1
-            total_vae_loss /= num_batches
-            total_gan_loss /= num_batches
-            msg = [f"Step: {step}"]
-            if vae:
-                msg.append(f"VAE loss: {total_vae_loss}")
-            if gan:
-                msg.append(f"GAN loss: {total_gan_loss}")
-            trange.write(", ".join(msg))
+            trange.write(f"Step: {step}, Loss: {total_score / num_batches}")
 
     # Plot samples
     fig, ax = plt.subplots()
@@ -473,7 +393,7 @@ def main(
         )
 
     ys_to_plot = jax.vmap(_interp)(ts_to_plot, ys_to_plot)[..., 0]
-    ys_sampled = jax.vmap(generator.sample)(
+    ys_sampled = jax.vmap(generator)(
         ts_to_plot, key=jrandom.split(sample_key, num_samples)
     )[..., 0]
     kwargs = dict(label="Real")
@@ -487,8 +407,7 @@ def main(
     ax.set_title(f"{num_samples} samples from both real and generated distributions.")
     fig.legend()
     fig.tight_layout()
-    fig.savefig(out_path)
-    return locals()  # TODO
+    fig.savefig("neural_sde_samples.png")
 
 
 if __name__ == "__main__":
