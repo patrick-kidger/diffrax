207aa4029406043c0f5ed0188d68f13ace538636
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2022-01-09 23:49:19 +0000

    removed unused BrownianInterval stub

--- diffrax/brownian/interval.py
+++
@@ -1,7 +0,0 @@
-from ..custom_types import PyTree, Scalar
-from .base import AbstractBrownianPath
-
-
-class BrownianInterval(AbstractBrownianPath):
-    def evaluate(self, t0: Scalar, t1: Scalar) -> PyTree:
-        ...  # TODO
