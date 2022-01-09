9ca6f71daa66f2101ac14411adf704bd990f81fb
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2022-01-09 20:49:13 +0000

    Added support for  to omega.

--- diffrax/misc/omega.py
+++ diffrax/misc/omega.py
@@ -1,4 +1,5 @@
 import operator
+from typing import Optional
 
 import jax
 import jax.numpy as jnp
@@ -10,32 +11,68 @@ class _Metaω(type):
 
 
 class ω(metaclass=_Metaω):
-    """Wraps jax.tree_map; e.g.
+    """Provides friendlier syntax for mapping with `jax.tree_map`.
 
-    ```python
-    (ω(a) + ω(b)).ω == jax.tree_map(operator.add, a, b)
-    ```
+    !!! example
 
-    Can also be initialised using the following __rpow__ syntax, which helps to
-    minimise the number of brackets:
+        ```python
+        (ω(a) + ω(b)).ω == jax.tree_map(operator.add, a, b)
+        ```
 
-    ```python
-    (a**ω + b**ω).ω == jax.tree_map(operator.add, a, b)
-    ```
+    !!! tip
+
+        To minimise the number of brackets, the following `__rpow__` syntax can be
+        used:
+
+        ```python
+        (a**ω + b**ω).ω == jax.tree_map(operator.add, a, b)
+        ```
+
+        This is entirely equivalent to the above.
     """
 
-    def __init__(self, value):
+    def __init__(self, value, is_leaf=None):
+        """
+        **Arguments:**
+
+        - `value`: The PyTree to wrap.
+        - `is_leaf`: An optional value for the `is_leaf` argument to `jax.tree_map`.
+
+        !!! note
+
+            The `is_leaf` argument cannot be set when using the `__rpow__` syntax for
+            initialisation.
+        """
         self.ω = value
+        self.is_leaf = is_leaf
 
     def __getitem__(self, item):
-        return jax.tree_map(lambda x: x[item], self.ω) ** ω
+        return ω(
+            jax.tree_map(lambda x: x[item], self.ω, is_leaf=self.is_leaf),
+            is_leaf=self.is_leaf,
+        )
 
     def call(self, fn):
-        return jax.tree_map(fn, self.ω) ** ω
+        return ω(jax.tree_map(fn, self.ω, is_leaf=self.is_leaf), is_leaf=self.is_leaf)
 
     @property
     def at(self):
-        return _ωUpdateHelper(self.ω)
+        return _ωUpdateHelper(self.ω, self.is_leaf)
+
+
+def _equal_code(fn1: Optional[callable], fn2: Optional[callable]):
+    """Checks whether fn1 and fn2 both have the same code.
+
+    It's essentially impossible to see if two functions are equivalent, so this won't,
+    and isn't intended, to catch every possible difference between fn1 and fn2. But it
+    should at least catch the common case that `is_leaf` is specified for one input and
+    not specified for the other.
+    """
+    sentinel1 = object()
+    sentinel2 = object()
+    code1 = getattr(getattr(fn1, "__code__", sentinel1), "co_code", sentinel2)
+    code2 = getattr(getattr(fn2, "__code__", sentinel1), "co_code", sentinel2)
+    return type(code1) == type(code2) and code1 == code2
 
 
 def _set_op_main(base, name: str, op: callable) -> callable:
@@ -43,9 +80,17 @@ def _set_op_main(base, name: str, op: callable) -> callable:
         if isinstance(other, ω):
             if jax.tree_structure(self.ω) != jax.tree_structure(other.ω):
                 raise ValueError("PyTree structures must match.")
-            return jax.tree_map(op, self.ω, other.ω) ** ω
+            if not _equal_code(self.is_leaf, other.is_leaf):
+                raise ValueError("`is_leaf` must match.")
+            return ω(
+                jax.tree_map(op, self.ω, other.ω, is_leaf=self.is_leaf),
+                is_leaf=self.is_leaf,
+            )
         elif isinstance(other, (bool, complex, float, int, jnp.ndarray)):
-            return jax.tree_map(lambda x: op(x, other), self.ω) ** ω
+            return ω(
+                jax.tree_map(lambda x: op(x, other), self.ω, is_leaf=self.is_leaf),
+                is_leaf=self.is_leaf,
+            )
         else:
             raise RuntimeError("Type of `other` not understood.")
 
@@ -74,17 +119,19 @@ for (name, op) in [
 
 
 class _ωUpdateHelper:
-    def __init__(self, value):
+    def __init__(self, value, is_leaf):
         self.value = value
+        self.is_leaf = is_leaf
 
     def __getitem__(self, item):
-        return _ωUpdateRef(self.value, item)
+        return _ωUpdateRef(self.value, item, self.is_leaf)
 
 
 class _ωUpdateRef:
-    def __init__(self, value, item):
+    def __init__(self, value, item, is_leaf):
         self.value = value
         self.item = item
+        self.is_leaf = is_leaf
 
     def get(self, **kwargs):
         value, item = self.ω
@@ -96,11 +143,24 @@ def _set_op_at(base, name: str, op: callable) -> callable:
         if isinstance(other, ω):
             if jax.tree_structure(self.value) != jax.tree_structure(other.ω):
                 raise ValueError("PyTree structures must match.")
-            return (
-                jax.tree_map(lambda x, y: op(x, self.item, y), self.value, other.ω) ** ω
+            if not _equal_code(self.is_leaf, other.is_leaf):
+                raise ValueError("is_leaf specifications must match.")
+            return ω(
+                jax.tree_map(
+                    lambda x, y: op(x, self.item, y),
+                    self.value,
+                    other.ω,
+                    is_leaf=self.is_leaf,
+                ),
+                is_leaf=self.is_leaf,
             )
         elif isinstance(other, (bool, complex, float, int, jnp.ndarray)):
-            return jax.tree_map(lambda x: op(x, self.item, other), self.value) ** ω
+            return ω(
+                jax.tree_map(
+                    lambda x: op(x, self.item, other), self.value, is_leaf=self.is_leaf
+                ),
+                is_leaf=self.is_leaf,
+            )
         else:
             raise RuntimeError("Type of `other` not understood.")
 
--- test/test_misc.py
+++ test/test_misc.py
@@ -1,6 +1,7 @@
 import diffrax
 import jax
 import jax.numpy as jnp
+import pytest
 
 from helpers import random_pytree, shaped_allclose, treedefs
 
@@ -36,3 +37,48 @@ def test_ω_inplace(getkey):
         b1 = ω(a).at[()].set(3).ω
         b2 = jax.tree_map(lambda ai: ai.at[()].set(3), a)
         assert shaped_allclose(b1, b2)
+
+        a2 = jax.tree_map(lambda x: x + 1, a)
+
+        b3 = ω(a).at[()].set(ω(a2)).ω
+        b4 = jax.tree_map(lambda ai, a2i: ai.at[()].set(a2i[()]), a, a2)
+        assert shaped_allclose(b3, b4)
+
+
+def test_ω_is_leaf(getkey):
+    ω = diffrax.misc.ω
+    for treedef in treedefs:
+        a = b = random_pytree(getkey(), treedef)
+        with pytest.raises(ValueError):
+            ω(a) + ω(b, is_leaf=lambda x: isinstance(x, int))
+        with pytest.raises(ValueError):
+            ω(a, is_leaf=lambda x: isinstance(x, int)) + ω(b)
+        with pytest.raises(ValueError):
+            ω(a, is_leaf=lambda x: isinstance(x, int)) + ω(
+                b, is_leaf=lambda x: isinstance(x, (int, str))
+            )
+
+        out = ω(a, is_leaf=lambda x: isinstance(x, int)) + ω(
+            b, is_leaf=lambda x: isinstance(x, int)
+        )
+        assert out.is_leaf(4)
+        assert not out.is_leaf("hi")
+
+        b = ω(a, is_leaf=lambda x: isinstance(x, int)).at[()].set(3)
+        assert out.is_leaf(4)
+        assert not out.is_leaf("hi")
+
+        a2 = jax.tree_map(lambda x: x + 1, a)
+
+        c = (
+            ω(a, is_leaf=lambda x: isinstance(x, int))
+            .at[()]
+            .set(ω(a2, is_leaf=lambda x: isinstance(x, int)))
+        )
+        assert c.is_leaf(4)
+        assert not c.is_leaf("hi")
+
+        with pytest.raises(ValueError):
+            ω(a, is_leaf=lambda x: isinstance(x, int)).at[()].set(ω(a2))
+        with pytest.raises(ValueError):
+            ω(a).at[()].set(ω(a2, is_leaf=lambda x: isinstance(x, int)))
