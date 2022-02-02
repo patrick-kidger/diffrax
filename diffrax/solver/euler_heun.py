81c47082d9f4b162c641bf5cdb326e84edeb351c
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2022-02-02 18:14:50 +0000

    Fixed Euler-Heun making an extra evaluation unnecessarily. (In practice it probably got CSE'd out anyway.)

--- diffrax/solver/euler_heun.py
+++ diffrax/solver/euler_heun.py
@@ -40,10 +40,11 @@ class EulerHeun(AbstractStratonovichSolver, AbstractSolver):
         drift, diffusion = terms
         dt = drift.contr(t0, t1)
         dW = diffusion.contr(t0, t1)
-        y_prime = (y0**ω + diffusion.vf_prod(t0, y0, args, dW) ** ω).ω
 
         f0 = drift.vf_prod(t0, y0, args, dt)
         g0 = diffusion.vf_prod(t0, y0, args, dW)
+
+        y_prime = (y0**ω + g0**ω).ω
         g_prime = diffusion.vf_prod(t0, y_prime, args, dW)
 
         y1 = (y0**ω + f0**ω + 0.5 * (g0**ω + g_prime**ω)).ω
