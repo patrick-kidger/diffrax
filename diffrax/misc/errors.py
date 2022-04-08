a29a549933b167527844164fb606c84f94b81909
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2022-04-08 16:00:38 +0100

    Fixed wrong errors with jaxlib>=0.3.5

--- diffrax/misc/errors.py
+++ diffrax/misc/errors.py
@@ -1,4 +1,4 @@
-from typing import Sequence, Type, Union
+from typing import Sequence, Union
 
 import jax.experimental.host_callback as hcb
 import jax.lax as lax
@@ -10,7 +10,6 @@ from .unvmap import unvmap_any
 def error_if(
     pred: Union[bool, Array[..., bool]],
     msg: str,
-    error_cls: Type[Exception] = ValueError,
 ) -> bool:
     """For use as part of validating inputs.
     Works even under JIT.
@@ -22,17 +21,16 @@ def error_if(
 
         f(jax.numpy.array(-1))
     """
-    branched_error_if(pred, 0, [msg], error_cls)
+    branched_error_if(pred, 0, [msg])
 
 
 def branched_error_if(
     pred: Union[bool, Array[..., bool]],
     index: Int,
     msgs: Sequence[str],
-    error_cls: Type[Exception] = ValueError,
 ) -> bool:
     def raises(_index):
-        raise error_cls(msgs[_index.item()])
+        raise RuntimeError(msgs[_index.item()])
 
     pred = unvmap_any(pred)
     lax.cond(pred, lambda: hcb.call(raises, index), lambda: None)
--- test/test_step_to.py
+++ test/test_step_to.py
@@ -36,22 +36,22 @@ def test_step_to_location():
 
     # ts[0] != t0
     ts = jnp.linspace(0.1, 2.0, 5)
-    with pytest.raises(ValueError):
+    with pytest.raises(RuntimeError):
         _test(ts)
 
     # ts[-1] != t1
     ts = jnp.linspace(0.0, 2.1, 5)
-    with pytest.raises(ValueError):
+    with pytest.raises(RuntimeError):
         _test(ts)
 
     # Not monotonic
     ts = jnp.array([0.0, 2.3, 2.0])
-    with pytest.raises(ValueError):
+    with pytest.raises(RuntimeError):
         _test(ts)
 
     # Reverse time
     ts = jnp.linspace(2.0, 0.0, 5)
-    with pytest.raises(ValueError):
+    with pytest.raises(RuntimeError):
         _test(ts)
     sol = _test(ts, flip=True)
     true_ys = jnp.array([1.0, 1.5, 2.25, 3.375, 5.0625])
