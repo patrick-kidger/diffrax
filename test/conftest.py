60772f3f74bc8edc79becac0d43677928ebfaa90
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-05-11 12:01:51 -0700

    foo

--- test/conftest.py
+++ test/conftest.py
@@ -28,19 +28,4 @@ def getkey():
 # Actions.
 @pytest.fixture(autouse=True)
 def clear_caches():
-    process = psutil.Process()
-    if process.memory_info().vms > 4 * 2**30:  # >4GB memory usage
-        jax.clear_backends()
-        for module_name, module in sys.modules.copy().items():
-            if module_name.startswith("jax"):
-                if module_name not in ["jax.interpreters.partial_eval"]:
-                    for obj_name in dir(module):
-                        obj = getattr(module, obj_name)
-                        if hasattr(obj, "cache_clear"):
-                            try:
-                                print(f"Clearing {obj}")
-                                if "Weakref" not in type(obj).__name__:
-                                    obj.cache_clear()
-                            except Exception:
-                                pass
-        gc.collect()
+    jax.clear_caches()
