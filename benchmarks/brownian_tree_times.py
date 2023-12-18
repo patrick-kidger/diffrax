b7dc6e7aa76ef5b77258b653be5c4e88710e9ccd
andyElking <andraz.jelincic@gmail.com>
2023-12-18 18:46:22 +0100

    Replaced abs with relu, and updated some comments

---
+++ benchmarks/brownian_tree_times.py
@@ -0,0 +1,181 @@
+import timeit
+from typing import cast, Optional, Union
+
+import equinox as eqx
+import equinox.internal as eqxi
+import jax
+import jax.lax as lax
+import jax.numpy as jnp
+import jax.random as jr
+import jax.tree_util as jtu
+from diffrax import AbstractBrownianPath, VirtualBrownianTree
+from diffrax._custom_types import RealScalarLike
+from jaxtyping import Array, Float, PRNGKeyArray, PyTree
+from lineax.internal import default_floating_dtype
+
+
+class _State(eqx.Module):
+    s: RealScalarLike
+    t: RealScalarLike
+    u: RealScalarLike
+    w_s: Float[Array, " *shape"]
+    w_t: Float[Array, " *shape"]
+    w_u: Float[Array, " *shape"]
+    key: PRNGKeyArray
+
+
+class OldVBT(AbstractBrownianPath):
+    t0: RealScalarLike
+    t1: RealScalarLike
+    tol: RealScalarLike
+    shape: PyTree[jax.ShapeDtypeStruct] = eqx.field(static=True)
+    key: PyTree[PRNGKeyArray]
+
+    def __init__(
+        self,
+        t0: RealScalarLike,
+        t1: RealScalarLike,
+        tol: RealScalarLike,
+        shape: Union[tuple[int, ...], PyTree[jax.ShapeDtypeStruct]],
+        key: PRNGKeyArray,
+        levy_area: str = "",
+    ):
+        del levy_area
+        self.t0 = t0
+        self.t1 = t1
+        self.tol = tol
+        self.shape = jax.ShapeDtypeStruct(shape, default_floating_dtype())
+        if any(
+            not jnp.issubdtype(x.dtype, jnp.inexact)
+            for x in jtu.tree_leaves(self.shape)
+        ):
+            raise ValueError(
+                "VirtualBrownianTree dtypes all have to be floating-point."
+            )
+        self.key = key
+
+    @eqx.filter_jit
+    def evaluate(
+        self,
+        t0: RealScalarLike,
+        t1: Optional[RealScalarLike] = None,
+        left: bool = True,
+        use_levy: bool = False,
+    ) -> PyTree[Array]:
+        del left, use_levy
+        t0 = eqxi.nondifferentiable(t0, name="t0")
+        if t1 is None:
+            return self._evaluate(t0)
+        else:
+            t1 = cast(RealScalarLike, eqxi.nondifferentiable(t1, name="t1"))
+            return jtu.tree_map(
+                lambda x, y: x - y,
+                self._evaluate(t1),
+                self._evaluate(t0),
+            )
+
+    def _evaluate(self, τ: RealScalarLike) -> PyTree[Array]:
+        map_func = lambda key, struct: self._evaluate_leaf(key, τ, struct)
+        return jtu.tree_map(map_func, self.key, self.shape)
+
+    def _brownian_bridge(self, s, t, u, w_s, w_u, key, shape, dtype):
+        mean = w_s + (w_u - w_s) * ((t - s) / (u - s))
+        var = (u - t) * (t - s) / (u - s)
+        std = jnp.sqrt(var)
+        return mean + std * jr.normal(key, shape, dtype)
+
+    def _evaluate_leaf(
+        self,
+        key,
+        τ: RealScalarLike,
+        struct: jax.ShapeDtypeStruct,
+    ) -> Array:
+        shape, dtype = struct.shape, struct.dtype
+
+        cond = self.t0 < self.t1
+        t0 = jnp.where(cond, self.t0, self.t1).astype(dtype)
+        t1 = jnp.where(cond, self.t1, self.t0).astype(dtype)
+
+        t0 = eqxi.error_if(
+            t0,
+            τ < t0,
+            "Cannot evaluate VirtualBrownianTree outside of its range [t0, t1].",
+        )
+        t1 = eqxi.error_if(
+            t1,
+            τ > t1,
+            "Cannot evaluate VirtualBrownianTree outside of its range [t0, t1].",
+        )
+        τ = jnp.clip(τ, t0, t1).astype(dtype)
+
+        key, init_key = jr.split(key, 2)
+        thalf = t0 + 0.5 * (t1 - t0)
+        w_t1 = jr.normal(init_key, shape, dtype) * jnp.sqrt(t1 - t0)
+        w_thalf = self._brownian_bridge(t0, thalf, t1, 0, w_t1, key, shape, dtype)
+        init_state = _State(
+            s=t0,
+            t=thalf,
+            u=t1,
+            w_s=jnp.zeros_like(w_t1),
+            w_t=w_thalf,
+            w_u=w_t1,
+            key=key,
+        )
+
+        def _cond_fun(_state):
+            return (_state.u - _state.s) > self.tol
+
+        def _body_fun(_state):
+            _key1, _key2 = jr.split(_state.key, 2)
+            _cond = τ > _state.t
+            _s = jnp.where(_cond, _state.t, _state.s)
+            _u = jnp.where(_cond, _state.u, _state.t)
+            _w_s = jnp.where(_cond, _state.w_t, _state.w_s)
+            _w_u = jnp.where(_cond, _state.w_u, _state.w_t)
+            _key = jnp.where(_cond, _key1, _key2)
+            _t = _s + 0.5 * (_u - _s)
+            _w_t = self._brownian_bridge(_s, _t, _u, _w_s, _w_u, _key, shape, dtype)
+            return _State(s=_s, t=_t, u=_u, w_s=_w_s, w_t=_w_t, w_u=_w_u, key=_key)
+
+        final_state = lax.while_loop(_cond_fun, _body_fun, init_state)
+
+        s = final_state.s
+        u = final_state.u
+        w_s = final_state.w_s
+        w_t = final_state.w_t
+        w_u = final_state.w_u
+        rescaled_τ = (τ - s) / (u - s)
+
+        A = jnp.array([[2, -4, 2], [-3, 4, -1], [1, 0, 0]])
+        coeffs = jnp.tensordot(A, jnp.stack([w_s, w_t, w_u]), axes=1)
+        return jnp.polyval(coeffs, rescaled_τ)
+
+
+key = jr.PRNGKey(0)
+t0, t1 = 0.3, 20.3
+ts = jnp.linspace(t0, t1, 1000000)
+
+
+def time_tree(tree_cls, tol, levy_area):
+    tree = tree_cls(t0=t0, t1=t1, tol=tol, shape=(100,), key=key, levy_area=levy_area)
+
+    @jax.jit
+    def run(_ts):
+        return jax.vmap(lambda _t: tree.evaluate(_t, use_levy=True))(_ts)
+
+    return min(
+        timeit.repeat(lambda: jax.block_until_ready(run(ts)), number=1, repeat=100)
+    )
+
+
+print(f"New Shallow   BM:  {time_tree(VirtualBrownianTree, 2**-3, ''):.3f}")
+
+print(f"New Shallow STLA:  {time_tree(VirtualBrownianTree, 2**-3, 'space-time'):.3f}")
+
+print(f"New Deep      BM:  {time_tree(VirtualBrownianTree, 2**-12, ''):.3f}")
+
+print(f"New Deep    STLA:  {time_tree(VirtualBrownianTree, 2**-12, 'space-time'):.3f}")
+
+print(f"Old Shallow:       {time_tree(OldVBT, 2**-3, ''):.3f}")
+
+print(f"Old Deep:          {time_tree(OldVBT, 2**-12, ''):.3f}")
--- test/test_brownian.py
+++ test/test_brownian.py
@@ -1,4 +1,6 @@
 import math
+from typing import Literal
+from typing_extensions import TypeAlias
 
 import diffrax
 import equinox as eqx
@@ -8,8 +10,10 @@ import jax.random as jr
 import jax.tree_util as jtu
 import pytest
 import scipy.stats as stats
+from diffrax._custom_types import LevyArea
 
 
+_Spline: TypeAlias = Literal["quad", "sqrt", "zero"]
 _vals = {
     int: [0, 2],
     float: [0.0, 2.0],
@@ -29,7 +33,7 @@ def _make_struct(shape, dtype):
 )
 @pytest.mark.parametrize("levy_area", ["", "space-time"])
 @pytest.mark.parametrize("use_levy", (False, True))
-def test_shape_and_dtype(ctr, levy_area, use_levy, getkey):
+def test_shape_and_dtype(ctr, levy_area: LevyArea, use_levy, getkey):
     t0 = 0
     t1 = 2
 
@@ -111,7 +115,7 @@ def test_shape_and_dtype(ctr, levy_area, use_levy, getkey):
 )
 @pytest.mark.parametrize("levy_area", ["", "space-time"])
 @pytest.mark.parametrize("use_levy", (False, True))
-def test_statistics(ctr, levy_area, use_levy):
+def test_statistics(ctr, levy_area: LevyArea, use_levy):
     # Deterministic key for this test; not using getkey()
     key = jr.PRNGKey(5678)
     keys = jr.split(key, 10000)
@@ -146,33 +150,34 @@ def test_statistics(ctr, levy_area, use_levy):
     assert pval > 0.1
 
 
-@pytest.mark.parametrize("levy_area", ["", "space-time"])
-@pytest.mark.parametrize("use_levy", (False, True))
-def test_conditional_statistics(levy_area, use_levy):
+def conditional_statistics(
+    levy_area: LevyArea, use_levy: bool, tol, spacing, spline: _Spline, min_num_points
+):
     key = jr.PRNGKey(5678)
     bm_key, sample_key, permute_key = jr.split(key, 3)
-
-    # Get >80 randomly selected points; not too close to avoid discretisation error.
+    # Get some randomly selected points; not too close to avoid discretisation error.
     t0 = 0.3
     t1 = 8.7
     ts = jr.uniform(sample_key, shape=(100,), minval=t0, maxval=t1)
     sorted_ts = jnp.sort(ts)
+    ts = []
     prev_ti = sorted_ts[0]
-    ts = [prev_ti]
+    ts.append(prev_ti)
     for ti in sorted_ts[1:]:
-        if ti < prev_ti + 2**-10:
+        if ti < prev_ti + spacing:
             continue
         prev_ti = ti
         ts.append(ti)
     ts = jnp.stack(ts)
-    assert len(ts) > 80
+    assert len(ts) > min_num_points
     ts = jr.permutation(permute_key, ts)
 
     # Get some random paths
     bm_keys = jr.split(bm_key, 10000)
+
     path = jax.vmap(
         lambda k: diffrax.VirtualBrownianTree(
-            t0=t0, t1=t1, shape=(), tol=2**-12, key=k, levy_area=levy_area
+            t0=t0, t1=t1, shape=(), tol=tol, key=k, levy_area=levy_area, _spline=spline
         )
     )(bm_keys)
 
@@ -182,6 +187,10 @@ def test_conditional_statistics(levy_area, use_levy):
         vals = jax.vmap(lambda p: p.evaluate(t0, ti, use_levy=use_levy))(path)
         out.append((ti, vals))
     out = sorted(out, key=lambda x: x[0])
+
+    pvals_w1 = []
+    pvals_w2 = []
+    pvals_h = []
 
     # Test their conditional statistics
     for i in range(1, len(ts) - 1):
@@ -211,7 +220,7 @@ def test_conditional_statistics(levy_area, use_levy):
         _, pval_w1 = stats.kstest(normalised_w1, stats.norm.cdf)
         # Raise if the failure is statistically significant at 10%, subject to
         # multiple-testing correction.
-        assert pval_w1 > 0.1 / (len(ts) - 2)
+        pvals_w1.append(pval_w1)
 
         if levy_area == "space-time" and use_levy:
             assert h_s is not None
@@ -234,7 +243,7 @@ def test_conditional_statistics(levy_area, use_levy):
             w_std2 = 2 * (a + b) / su
             normalised_w2 = (w_r - w_mean2) / w_std2
             _, pval_w2 = stats.kstest(normalised_w2, stats.norm.cdf)
-            assert pval_w2 > 0.1 / (len(ts) - 2)
+            pvals_w2.append(pval_w2)
 
             # Check h_r|(w_s, w_u, h_s, h_u)
             h_mean = (
@@ -247,7 +256,81 @@ def test_conditional_statistics(levy_area, use_levy):
             h_std = math.sqrt(h_var)
             normalised_hh = (h_r - h_mean) / h_std
             _, pval_h = stats.kstest(normalised_hh, stats.norm.cdf)
-            assert pval_h > 0.1 / (len(ts) - 2)
+            pvals_h.append(pval_h)
+    return jnp.array(pvals_w1), jnp.array(pvals_w2), jnp.array(pvals_h)
+
+
+@pytest.mark.parametrize("levy_area", ["", "space-time"])
+@pytest.mark.parametrize("use_levy", (False, True))
+def test_conditional_statistics(levy_area: LevyArea, use_levy):
+    pvals_w1, pvals_w2, pvals_h = conditional_statistics(
+        levy_area, use_levy, tol=2**-8, spacing=2**-10, spline="sqrt", min_num_points=90
+    )
+    assert jnp.all(pvals_w1 > 0.1 / pvals_w1.shape[0])
+    if levy_area == "space-time" and use_levy:
+        assert jnp.all(pvals_w2 > 0.1 / pvals_w2.shape[0])
+        assert jnp.all(pvals_h > 0.1 / pvals_h.shape[0])
+    else:
+        assert len(pvals_w2) == 0
+        assert len(pvals_h) == 0
+
+
+def _levy_area_spline():
+    for levy_area in ("", "space-time"):
+        for spline in ("quad", "sqrt", "zero"):
+            if levy_area == "space-time" and spline == "quad":
+                continue
+            yield levy_area, spline
+
+
+@pytest.mark.parametrize("levy_area,spline", _levy_area_spline())
+@pytest.mark.parametrize("use_levy", (False, True))
+def test_spline(levy_area: LevyArea, use_levy, spline):
+    if levy_area == "space-time" and spline == "quad":
+        pytest.skip("Quad spline is not implemented for space-time Levy area")
+
+    pvals_w1, pvals_w2, pvals_h = conditional_statistics(
+        levy_area,
+        use_levy=use_levy,
+        tol=2**-4,
+        spacing=2**-5,
+        spline=spline,
+        min_num_points=50,
+    )
+
+    if spline == "sqrt":
+        # For the correct spline, make sure that all p-values are above
+        # 0.1 (subject to multiple-testing correction) and the average
+        # p-value is above 0.3.
+        def pred(pvals):
+            return jnp.min(pvals) > 0.1 / pvals.shape[0] and jnp.mean(pvals) > 0.3
+
+    else:
+        # make sure that for incorrect splines at least one p-value is
+        # below 0.001 (subject to multiple-testing correction) and the
+        # average p-value is below 0.03 (i.e. at least 10x smaller than
+        # for the correct spline).
+        def pred(pvals):
+            return jnp.min(pvals) < 0.001 / pvals.shape[0] and jnp.mean(pvals) < 0.03
+
+    if levy_area == "space-time" and use_levy:
+        assert pred(pvals_w2)
+        assert pred(pvals_h)
+    else:
+        assert len(pvals_w2) == 0
+        assert len(pvals_h) == 0
+        if levy_area == "":
+            assert pred(pvals_w1)
+        elif spline == "sqrt":  # levy_area == "space-time"
+            assert pred(pvals_w1)
+        else:  # levy_area == "space-time" and spline == "zero"
+            # We need a milder upper bound on jnp.mean(pvals_w1) because
+            # the presence of space-time Levy area gives W_r (i.e. the output
+            # of the Brownian path) a variance very close to the correct one,
+            # even when the spline is wrong. In pvals_w2 the influence of the
+            # Levy area is subtracted in the mean, so we can use a stricter test.
+            n = pvals_w1.shape[0]
+            assert jnp.min(pvals_w1) < 0.01 / n and jnp.mean(pvals_w1) < 0.2
 
 
 def test_levy_area_reverse_time():
