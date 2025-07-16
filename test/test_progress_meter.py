e91c1e6569b708a67f99ae617d15dc8c560f25a8
Jonathan Brodrick <jonathanbrodrick@gmail.com>
2025-07-16 02:16:45 +0100

    Improve ConstantStepSize incrementation

--- test/test_progress_meter.py
+++ test/test_progress_meter.py
@@ -53,31 +53,31 @@ def test_text_progress_meter(capfd):
             t1=5,
             dt0=0.01,
             y0=1.0,
-            progress_meter=diffrax.TextProgressMeter(minimum_increase=0.1),
+            progress_meter=diffrax.TextProgressMeter(minimum_increase=0.0999),
         )
 
     solve(2.0)
     jax.effects_barrier()
     captured = capfd.readouterr()
-    expected = "0.00%\n10.33%\n20.67%\n31.00%\n41.33%\n51.67%\n62.00%\n72.33%\n82.67%\n93.00%\n100.00%\n"  # noqa: E501
+    expected = "%\n".join(f"{x:.2f}" for x in jnp.linspace(0, 100, num=11)) + "%\n"
     assert captured.out == expected
 
     jax.vmap(solve)(jnp.arange(3.0))
     jax.effects_barrier()
     captured = capfd.readouterr()
-    expected = "0.00%\n10.00%\n20.00%\n30.00%\n40.00%\n50.20%\n60.40%\n70.60%\n80.80%\n91.00%\n100.00%\n"  # noqa: E501
+    expected = "%\n".join(f"{x:.2f}" for x in jnp.linspace(0, 100, num=11)) + "%\n"
     assert captured.out == expected
 
     jax.jit(solve)(2.0)
     jax.effects_barrier()
     captured = capfd.readouterr()
-    expected = "0.00%\n10.33%\n20.67%\n31.00%\n41.33%\n51.67%\n62.00%\n72.33%\n82.67%\n93.00%\n100.00%\n"  # noqa: E501
+    expected = "%\n".join(f"{x:.2f}" for x in jnp.linspace(0, 100, num=11)) + "%\n"
     assert captured.out == expected
 
     jax.jit(jax.vmap(solve))(jnp.arange(3.0))
     jax.effects_barrier()
     captured = capfd.readouterr()
-    expected = "0.00%\n10.00%\n20.00%\n30.00%\n40.00%\n50.20%\n60.40%\n70.60%\n80.80%\n91.00%\n100.00%\n"  # noqa: E501
+    expected = "%\n".join(f"{x:.2f}" for x in jnp.linspace(0, 100, num=11)) + "%\n"
     assert captured.out == expected
 
 
