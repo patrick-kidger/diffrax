0a2eebfc7fd965b699f8982d434df7f3c1c15c2a
Alexander de Ranitz <alexanderderanitz@gmail.com>
2026-02-22 11:39:54 +0100

    Add pytest for bitcasting to smaller type
    This checks the intended behaviour of mapping nearby numbers to distinct values when downcasting to a smaller dtype.

--- test/test_misc.py
+++ test/test_misc.py
@@ -9,3 +9,22 @@ def test_fill_forward():
     out_ = jnp.array([jnp.nan, 0.0, 1.0, 1.0, 1.0, 2.0, 2.0])
     fill_in = diffrax._misc.fill_forward(in_[:, None])
     assert tree_allclose(fill_in, out_[:, None], equal_nan=True)
+
+
+def test_force_bitcast_convert_type():
+    val_1 = jnp.float64(1e6)
+    val_2 = jnp.float64(1e6 + 1e-4)
+
+    # Val_1 and val_2 are different as float64,
+    # but would be the same if naively downcast to float32.
+    assert val_1 != val_2
+    assert val_1.astype(jnp.int32) == val_2.astype(jnp.int32)
+
+    val_1_cast = diffrax._misc.force_bitcast_convert_type(val_1, jnp.int32)
+    val_2_cast = diffrax._misc.force_bitcast_convert_type(val_2, jnp.int32)
+
+    assert val_1_cast.dtype == jnp.int32
+    assert val_2_cast.dtype == jnp.int32
+
+    # Bitcasted values should be different in the smaller type
+    assert val_1_cast != val_2_cast
